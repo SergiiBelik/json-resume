c884e5d1f4f9307f7eb87ad1f7239c2bff7ed349
Sergii B <sergii.biletskyi@tulip.com>
2019-12-01 12:04:20 -0500

    minor formatting

--- resume.js
+++ resume.js
@@ -5,29 +5,29 @@ $.getJSON('resume.json', function(data) {
 
 
 function render(jsonResume){
-	$('.name').text(jsonResume.basics.name);
-	$(".email i").text(jsonResume.basics.email);
-	$(".phone-number i").text(jsonResume.basics.phone);
+	$(".name").text(jsonResume.basics.name);
+	$(".email i span").text(jsonResume.basics.email);
+	$(".phone-number i span").text(jsonResume.basics.phone);
 	$(".linkedIn i a").text(jsonResume.basics.profiles[0].username).attr({
 		href: jsonResume.basics.profiles[0].url,
-		target: '_blank'
-	});;
+		target: "_blank"
+	});
 	$(".gitHub i a").text(jsonResume.basics.profiles[1].username).attr({
 		href: jsonResume.basics.profiles[1].url,
 		target: "_blank"
 	});
-	$('<img>').attr({
+	$("<img>").attr({
 		src: jsonResume.basics.picture,
-		alt: 'photo',
-	}).addClass('photo').appendTo('.photo');
-	$('.summary').text(jsonResume.basics.summary);
+		alt: "photo"
+	}).addClass("photo").appendTo(".photo");
+	$(".summary").text(jsonResume.basics.summary);
 	$.each(jsonResume.education, function(index, el) {
-		$('.education').append(el.institution + " · " + el.startDate + " - " + el.endDate + "<br>" + el.studyType + " in " + el.area)
+		$(".education").append(el.institution + " · " + el.startDate + " - " + el.endDate + "<br>" + el.studyType + " in " + el.area);
 	});
 	$(jsonResume.skills).each(function(index, value){
 		$(".skills").append("<li>"+value.keywords.join(", ")+"</li>");
 		}
-	)
+	);
 	$.each(jsonResume.publications, function(index, el) {
 		$("<a>"+el.name+"</a> <span>" + el.summary + "<br>" + el.website+"</span><br><br>").attr({
 			href: el.website,
@@ -36,23 +36,19 @@ function render(jsonResume){
 	});
 	$.each(jsonResume.courses, function(index, el) {
 		$("<li>"+el.name+"</li>").css({
-			"font-weight": 'normal'
+			"font-weight": "normal"
 		}).appendTo('.listOfCourses');
 	});
 	$.each(jsonResume.interests, function(index, el) {
-
-		$("<div class='categoryOfActivities'></div>").text(el.name + ":").appendTo('.activities');
-		$("<div class='listOfActivities'></div>").text(el.keywords.join(", ")).appendTo('.activities');
-		$("<br>").appendTo('.activities')
+		$("<div class='categoryOfActivities'></div>").text(el.name + ":").appendTo(".activities");
+		$("<div class='listOfActivities'></div>").text(el.keywords.join(", ")).appendTo(".activities");
+		$("<br>").appendTo(".activities");
 	});
 	$.each(jsonResume.work, function(index, value){
 		$("<div class='dates'>"+value.startDate+ " - "+value.endDate+"</div>").appendTo(".employementWrapper");
 		$("<div class='description'><b>"+value.company+"</b> · "+value.location+"<br><i><b><span class='title'>"+value.position+"</span></b></i><ul class='highlights'></ul></div>").appendTo(".employementWrapper");
 		$.each(value.highlights, function(i, highlight){
-			$("<li>"+highlight+"</li>").appendTo('.highlights:last');
+			$("<li>"+highlight+"</li>").appendTo(".highlights:last");
 		})
 	});
 }
-
-
-// render(jsonResume);
